25d4976ff6b4365a0317cc0c93d072825afadfca
Margarita <kirilchuk.margarita@gmail.com>
2020-05-23 18:07:19 +0300

    Changes added

--- src/main/java/ru/netology/domain/TicketInformationByTime.java
+++ src/main/java/ru/netology/domain/TicketInformationByTime.java
@@ -7,20 +7,13 @@ import lombok.Setter;
 
 import java.util.Comparator;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 public class TicketInformationByTime implements Comparator<TicketInformation> {
-        private int id;
-        private int price;
-        private String from;
-        private String to;
-        private double time;
 
 
     @Override
     public int compare(TicketInformation o1, TicketInformation o2) {
+        // для time используется тип double
         return (int) (o1.getTime() - o2.getTime());
+
     }
 }
